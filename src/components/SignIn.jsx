cc9ba4f8ab93b7ccb56f7229419e0e9570316224
Guilherme Santos <santosgui678@gmail.com>
2024-05-26 10:29:43 -0300

    Exercises 10.13 and 10.14

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,6 +2,8 @@ import { Pressable, View, StyleSheet } from 'react-native';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
+import useSignIn from '../hooks/useSignIn';
+
 const validationSchema = yup.object().shape({
   username: yup.string().required('Username is required'),
   password: yup.string().required('Password is required')
@@ -62,8 +64,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return <SignInForm onSubmit={onSubmit}/>
